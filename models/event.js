aaf41f609ed88d3daadd308945f40f52d70fd2eb
Purvil Bambharolia <purvil12c@gmail.com>
2019-06-10 14:07:10 -0400

    Added user  model

--- models/event.js
+++ models/event.js
@@ -4,7 +4,11 @@ const eventSchema = new Schema({
   title: {type: 'String', required: true},
   price: {type: 'Number', required: true},
   date: {type: 'Date', required: true},
-  description: {type: 'String', required: true}
+  description: {type: 'String', required: true},
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 
 });
 
